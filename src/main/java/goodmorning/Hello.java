1a993e93696a942e39c36552781563fa979b64ff
navisatebi <navis@ebi.ac.uk>
2017-11-24 12:29:19 +0000

    ci check

--- src/main/java/goodmorning/Hello.java
+++ src/main/java/goodmorning/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		System.out.println("Hello World , Good Morning");
 		System.out.println("Hello World , Good Morning   Again");
 		System.out.println("Hello World , Good Morning   Again");
+		System.out.println("Hello World , Good Morning   Again");
 	}
 
 }
