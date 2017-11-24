09539a3ac8b011ffbf1c1dbf9699a2ef1d2e3ee9
navisatebi <navis@ebi.ac.uk>
2017-11-24 11:28:52 +0000

    check commit

--- src/main/java/goodmorning/Hello.java
+++ src/main/java/goodmorning/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World , Good Morning");
+		System.out.println("Hello World , Good Morning   Again");
 	}
 
 }
