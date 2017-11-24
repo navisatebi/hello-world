94a09c04c7caba8e75fce3a71d5ca67a545739b1
navisatebi <navis@ebi.ac.uk>
2017-11-24 12:26:52 +0000

    checking ci

--- src/main/java/goodmorning/Hello.java
+++ src/main/java/goodmorning/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World , Good Morning");
 		System.out.println("Hello World , Good Morning   Again");
+		System.out.println("Hello World , Good Morning   Again");
 	}
 
 }
